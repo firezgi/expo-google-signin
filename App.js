401f9f1fd35235346c8e8534e60e56924663dd90
Habteab <keshi2017@gmail.com>
2022-05-17 14:27:55 -0700

    chore(app): delete console log

--- App.js
+++ App.js
@@ -17,7 +17,6 @@ export default function App() {
   useEffect(() => {
     if (response?.type === 'success') {
       const { authentication,type } = response;
-      console.log(type)
       setLoggedIn(type)
       }
   }, [response]);
